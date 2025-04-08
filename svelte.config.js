75b73babb19371a45630e0e4fbfb1239ee5e6011
rimico2099 <ravjoth.brar@gmail.com>
2025-04-08 19:10:28 +0100

    Create svelte.config.js

---
+++ svelte.config.js
@@ -0,0 +1,24 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported, use adapter-static
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+      precompress: false,
+      strict: true
+    }),
+    paths: {
+      // Updated to match your GitHub repository name "friends"
+      base: process.env.NODE_ENV === 'production' ? '/friends' : ''
+    }
+  }
+};
+
+export default config;
